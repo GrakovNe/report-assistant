045750368d68954104390715936feac99a061e99
Max Grakov <mgrakov@lineate.com>
2020-06-04 20:09:43 +0600

    initial commit

---
+++ src/main/kotlin/eco/omsk/report/assistant/ReportAssistant.kt
@@ -0,0 +1,11 @@
+package eco.omsk.report.assistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportAssistant
+
+fun main(args: Array<String>) {
+    runApplication<ReportAssistant>(*args)
+}
